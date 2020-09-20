e34f59970c5e2264d9f58e50d900ade8810a30ca
vishnu <vishnuvijayakumar125@gmail.com>
2020-09-20 13:18:11 +1000

    API and test_cases add

---
+++ starter/backend/test_flaskr.py
@@ -0,0 +1,104 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format('vishnu@localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        #binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+
+    def test_404_invalid_questions_page(self):
+        res = self.client().get('/questions?page=4')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,404)
+        self.assertEqual(data['success'],False)
+        self.assertEqual(data['error'],404)
+
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['categories'])
+
+    def test_post_categories(self):
+        res = self.client().post('/categories', json={'type':'Programming'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,405)
+        self.assertEqual(data['success'],False)
+        self.assertEqual(data['error'],405)
+
+    def test_404_delete_invalid_questions(self):
+        res = self.client().delete('/questions/300')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,404)
+        self.assertEqual(data['success'],False)
+
+    def test_404_get_invalid_categories_questions(self):
+        res = self.client().get('/categories/100/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,404)
+        self.assertEqual(data['success'],False)
+
+    def test_post_searchTerm(self):
+        res = self.client().post('/questions', json={'searchTerm':'What'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['questions'])
+
+    def test_post_searchTerm(self):
+        res = self.client().post('/questions', json={'question':'What'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,400)
+        self.assertEqual(data['success'],False)
+        
+        
+
+    
+       
+
+   
+       
+
+    
+
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
